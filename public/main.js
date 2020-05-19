5f8f659fe883339ffaeb37fa51b730072a118478
Yudi Yamane <yudiyamane@gmail.com>
2020-05-19 15:52:46 -0300

    interface looks more squared

--- public/main.js
+++ public/main.js
@@ -11,8 +11,8 @@ const player1 = {id: 'yudi beckham', symbol: 'Y'}
 
 game.state.currentBoardIndex = 4 // como definir que a partida vai começar? 
 
-game.setPlayer(0,player0); //quem que deveria fazer isso???
-game.setPlayer(1,player1);
+game.setPlayer(0, player0); //quem que deveria fazer isso???
+game.setPlayer(1, player1);
 
 game.state.currentPlayer =  game.selectRandomPlayer();// isso não deveria estar numa função de iniciar a partida?
 
