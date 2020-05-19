fc1f2f4c6dae4bbde9a423eb903029f684ccc95c
Yudi Yamane <yudiyamane@gmail.com>
2020-05-19 15:10:59 -0300

    reindent

--- public/main.js
+++ public/main.js
@@ -1,31 +1,31 @@
 import createGame from './modules/game.js';
 import createRenderer from './modules/renderer.js'
 
-  const game = createGame();
-  const renderer = createRenderer(document)
+const game = createGame();
+const renderer = createRenderer(document)
 
-  game.setUp();
-  const player0 = {id: 'david beckham', symbol: 'X'}
-  const player1 = {id: 'yudi beckham', symbol: 'Y'}
-  game.state.currentBoardIndex = 4 // como definir que a partida vai começar? 
+game.setUp();
 
+const player0 = {id: 'david beckham', symbol: 'X'}
+const player1 = {id: 'yudi beckham', symbol: 'Y'}
 
+game.state.currentBoardIndex = 4 // como definir que a partida vai começar? 
 
-  game.setPlayer(0,player0); //quem que deveria fazer isso???
-  game.setPlayer(1,player1);
+game.setPlayer(0,player0); //quem que deveria fazer isso???
+game.setPlayer(1,player1);
 
-  game.state.currentPlayer =  game.selectRandomPlayer();// isso não deveria estar numa função de iniciar a partida?
+game.state.currentPlayer =  game.selectRandomPlayer();// isso não deveria estar numa função de iniciar a partida?
 
-  renderer.initialize(game.state)
+renderer.initialize(game.state)
 
-  renderer.subject.subscribe({
-    topic: 'click',
-    observerFunction: game.executeTurn
-  });
+renderer.subject.subscribe({
+  topic: 'click',
+  observerFunction: game.executeTurn
+});
 
-  game.subject.subscribe({
-    topic: 'newMove',
-    observerFunction: renderer.render
-  });
+game.subject.subscribe({
+  topic: 'newMove',
+  observerFunction: renderer.render
+});
 
-  renderer.render(game.state);
+renderer.render(game.state);
