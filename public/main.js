3589d16f209e6d55041bc5f5999d707a0f985d09
Edmundo-Ribeiro <jtvedy@gmail.com>
2020-05-22 04:27:23 -0300

    removed responsability of choosing the first board, added some temporary subscriptions and moved some lines to better places

--- public/main.js
+++ public/main.js
@@ -1,31 +1,41 @@
 import createGame from './modules/game.js';
 import createRenderer from './modules/renderer.js'
 
+  const player0 = {id: 'david beckham', symbol: 'X'};
+  const player1 = {id: 'yudi beckham', symbol: 'Y'};
+
   const game = createGame();
-  const renderer = createRenderer(document)
+  const renderer = createRenderer(document);
 
   game.setUp();
-  const player0 = {id: 'david beckham', symbol: 'X'}
-  const player1 = {id: 'yudi beckham', symbol: 'Y'}
-  game.state.currentBoardIndex = 4 // como definir que a partida vai começar? 
-
-
-
   game.setPlayer(0,player0); //quem que deveria fazer isso???
   game.setPlayer(1,player1);
-
   game.state.currentPlayer =  game.selectRandomPlayer();// isso não deveria estar numa função de iniciar a partida?
 
-  renderer.initialize(game.state)
+  renderer.initialize(game.state);
 
   renderer.subject.subscribe({
-    topic: 'click',
-    observerFunction: game.executeTurn
+      topic: 'click',
+      observerFunction: game.executeTurn
   });
-
+  
   game.subject.subscribe({
-    topic: 'newMove',
-    observerFunction: renderer.render
+      topic: 'newMove',
+      observerFunction: renderer.render
+  });
+  game.subject.subscribe({
+      topic: 'hasToChooseBoard',
+      observerFunction: renderer.render
+  }); 
+  game.subject.subscribe({
+      topic: 'endGame',
+      observerFunction: renderer.endedGame
   });
 
   renderer.render(game.state);
+  
+
+
+
+
+
